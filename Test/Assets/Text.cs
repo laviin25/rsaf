9203ef4b68791802962db8726ab814380549505b
laviin25 <laviin25@gmail.com>
2019-05-06 14:53:00 +0800

    vuforia

---
+++ Test/Assets/Text.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    public string txtFile = "test";
+    string txtContents;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset txtAssets = (TextAsset)Resources.Load(txtFile);
+        txtContents = txtAssets.text;
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        GUILayout.Label(txtContents);
+    }
+}
---
+++ Test/Assets/TextBoxManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class TextBoxManager : MonoBehaviour
+{
+
+    public GameObject textBox;
+    public UnityEngine.UI.Text theText;
+    public TextAsset textFile;
+    public string[] textLines;
+    public int currentLine;
+    public int endAtLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(textFile!=null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+        if (endAtLine == 0)
+            endAtLine = textLines.Length-1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theText = GetComponent<UnityEngine.UI.Text>();
+        theText.text = textLines[currentLine];
+        
+    }
+}
